fe3bde1b1f8bfd51096744a724af086ce82cf8ad
Artem Bo <artembo.dev@gmail.com>
2022-10-04 12:41:19 -0700

    refactor code and update README.md

--- main.py
+++ main.py
@@ -4,44 +4,54 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 import datetime
 import pandas
+import os
+from dotenv import load_dotenv
+
+YEAR_OF_FOUNDATION = 1920
 
 
-def year_ending(year):
-    endings = ["лет", "год", "года"]
-    num = year % 100
-    if 21 > num > 4:
-        return f"{year} {endings[0]}"
-    num = num % 10
-    if num == 1:
-        return f"{year} {endings[1]}"
-    if 1 < num < 5:
-        return f"{year} {endings[2]}"
-    return f"{year} {endings[0]}"
+def format_year_spelling(year):
+    if 10 < year / 100 < 20:
+        return f"{year} лет"
+    elif year % 100 == 1:
+        return f"{year} год"
+    elif 1 < year % 100 < 5:
+        return f"{year} годa"
+    else:
+        return f"{year} лет"
 
 
-current_year = datetime.datetime.now().year
-winery_age = current_year - 1920
+def main():
+    load_dotenv()
+    data_xls = os.getenv('WINE_DATA_XLS', default='wine.xlsx')
 
-drinks_data_frame = pandas.read_excel("wine.xlsx", keep_default_na=False)
-drinks_records = drinks_data_frame.to_dict('records')
+    drinks_data_frame = pandas.read_excel(data_xls, keep_default_na=False)
+    drinks_records = drinks_data_frame.to_dict('records')
 
-drinks = defaultdict(list)
-for drink in drinks_records:
-    drinks[drink['Категория']].append(drink)
+    drinks = defaultdict(list)
+    for drink in drinks_records:
+        drinks[drink['Категория']].append(drink)
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
-rendered_page = template.render(
-    winery_age=year_ending(winery_age),
-    drinks=drinks
-)
+    current_year = datetime.datetime.now().year
+    winery_age = current_year - YEAR_OF_FOUNDATION
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        winery_age=format_year_spelling(winery_age),
+        drinks=drinks
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
